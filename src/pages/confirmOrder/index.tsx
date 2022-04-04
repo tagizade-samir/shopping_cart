9a7f6934a2bce3603fd32a4196ddd3854d12bbf2
tagizade-samir <tagizade.samir@gmail.com>
2022-04-05 00:04:28 +0400

    fix

--- src/pages/confirmOrder/index.tsx
+++ src/pages/confirmOrder/index.tsx
@@ -1,7 +1,7 @@
 import { Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import { NextRouter, useRouter } from 'next/router';
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import MainButton from '../../components/mainButton';
 import { AppDispatch } from '../../modules/redux';
@@ -10,8 +10,20 @@ import { clearCartAC } from '../../modules/redux/reducers/cart/actions';
 const ConfirmOrder: FC<{}> = () => {
     const router: NextRouter = useRouter();
     const { data }: any = router.query;
-    const products = JSON.parse(data);
     const dispatch: AppDispatch = useDispatch();
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        if (data) {
+            try {
+                const result = JSON.parse(data);
+                setProducts(result);
+            } catch(e) {
+                console.warn('Error parse products: ', e);
+            }
+        }
+    }, []);
 
     const handleConfirmOrder = () => {
         dispatch(clearCartAC());
