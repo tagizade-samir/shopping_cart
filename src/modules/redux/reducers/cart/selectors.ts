b0217253269a2e964c7e4e5e619af337233cf1e0
tagizade-samir <tagizade.samir@gmail.com>
2022-04-05 02:15:05 +0400

    fix

--- src/modules/redux/reducers/cart/selectors.ts
+++ src/modules/redux/reducers/cart/selectors.ts
@@ -4,5 +4,5 @@ import { IProduct } from "../products/types";
 export const selectCartItems = ((state: RootState): Array<IProduct> | [] => state.cart.items);
 
 export const selectCartItemsIds = ((state: RootState): Array<number> | [] => {
-    return state.cart.items.length ? state.cart.items.map(i => i.id) : [];
+    return state.cart.items.length ? state.cart.items.map((item: IProduct) => item.id) : [];
 });
